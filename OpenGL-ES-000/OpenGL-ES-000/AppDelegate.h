023283c65a7e30b4db81a77c7b6ec24eb71d3b0e
Alan <softwarefaith@126,con>
2016-10-08 18:21:46 +0800

    OpenGL

---
+++ OpenGL-ES-000/OpenGL-ES-000/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGL-ES-000
+//
+//  Created by 蔡杰Alan on 16/10/8.
+//  Copyright © 2016年 Allan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGL-ES-000/OpenGL-ES-000/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  OpenGL-ES-000
+//
+//  Created by 蔡杰Alan on 16/10/8.
+//  Copyright © 2016年 Allan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+#import <OpenGLES/EAGL.h>
+#import <OpenGLES/ES3/gl.h>
+#import <GLKit/GLKit.h>
+
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
